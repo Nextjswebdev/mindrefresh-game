de57a1ff75460cf30751b7666fc4312cd1a0539a
Mohd Khan <oliwertwist9@gmail.com>
2024-06-21 13:00:38 +0530

    just lala

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,20 @@ const nextConfig = {
     images: {
         domains: ['media.giphy.com'],
       },
-      webpack(config) {
-        config.module.rules.push({
-          test: /\.(mp3|wav)$/,
-          use: {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]',
-              publicPath: `/_next/static/files`,
-              outputPath: 'static/files',
-            },
-          },
-        });
-    
-        return config;
-      },
+      module: {
+        rules: [
+          {
+            test: /\.mp3$/,
+            use: {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'audios/'
+              }
+            }
+          }
+        ]
+      }
 };
 
 export default nextConfig;
